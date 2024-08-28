08ccdcf961519861362718525c95ce19d51e06d1
Romeo2520 <romeo.gossou-bah@epitech.eu>
2024-08-28 18:44:59 +0000

    ok

---
+++ connect4.js
@@ -0,0 +1,145 @@
+class Connect4 {
+    constructor(selector) {
+        this.ROWS = 6;
+        this.COLS = 7;
+        this.player = 'red';
+        this.selector = selector;
+        this.createGrid(); 
+        this.setupEventListeners()
+        this.isGameOver = false;
+        this.onPlayerMove = function() {}
+    }
+// create a grid 
+   createGrid() {
+        const $board = $(this.selector);
+        $board.empty();
+        this.isGameOver = false;
+        this.player = 'red'
+        for (let row = 0; row < this.ROWS; row++){
+            const $row = $('<div>')
+            .addClass('row');
+            for (let col = 0; col < this.COLS; col++){
+                const $col = $('<div>')
+                .addClass('col empty')
+                .attr('data-col', col)// id generator per col
+                .attr('data-row', row);//id generator per row 
+            $row.append($col);
+        }
+        $board.append($row);
+    }
+    }
+    setupEventListeners() {
+        const $board = $(this.selector);
+        const that = this;
+
+        function findLastEmptyCell(col) {//get the last empty cell
+            const cells = $ (`.col[data-col='${col}']`);
+            for (let i = cells.length - 1; i >= 0; i --){
+                const $cell = $(cells[i]);
+                if ($cell.hasClass('empty')) {
+                    return $cell;
+                }
+            }
+            return nulll
+
+        }
+
+        $board.on('mouseenter', '.col.empty', function(){
+            if(that.isGameOver) return;
+            const col = $(this).data('col');
+            const $lastEmptyCell = findLastEmptyCell(col)// get the last empty cell 
+            $lastEmptyCell.addClass(`next-${that.player}`);
+        
+        })
+
+
+         $board.on('mouseleave', '.col', function(){
+            $('.col').removeClass(`next-${that.player}`);
+         })
+
+
+         $board.on ('click', '.col.empty', function(){
+            if(that.isGameOver) return;
+            const col = $(this).data('col');
+            // const row = $(this).data('row');
+            const $lastEmptyCell = findLastEmptyCell(col)// get the last empty cell 
+            $lastEmptyCell.removeClass(`empty next-${that.player}`);
+            $lastEmptyCell.addClass(that.player);
+            $lastEmptyCell.data('player', that.player); 
+
+            // console.log(row, col);
+            const winner = that.checkForWinner(
+            $lastEmptyCell.data('row'),
+            $lastEmptyCell.data('col')
+        )
+            if (winner) {
+                that.isGameOver = true
+                alert(`Game Over ! Player ${that.player} has won!`);
+                $('.col.empty').removeClass('empty');
+                return;  
+            }
+
+            that.player = (that.player === 'red') ? 'black' : 'red';
+            that.onPlayerMove(); 
+            $(this).trigger('mouseenter');
+
+         });
+    }
+    checkForWinner(row, col) {
+        const that = this;
+
+        function $getCell(i, j) {
+            return $(`.col[data-row='${i}'][data-col='${j}']`);
+        }
+
+        function checkDirection(direction) {
+            // debugger; 
+            let total = 0;
+            let i = row + direction.i;
+            let j = col + direction.j;
+            let $next = $getCell(i, j);
+            while (i >= 0 && 
+                i < that.ROWS &&
+                j >= 0 &&
+                j < that.COLS &&
+                $next.data('player') === that.player) {
+                    total ++;
+                    i += direction.i;
+                    j += direction.j;
+                    $next = $getCell(i, j)
+                }
+                return total;
+        }
+
+        function checkWin(A, B) {
+            const total = 1 + checkDirection(A) + checkDirection(B);
+            if (total >= 4) {
+                return that.player;
+            }else {
+                return null 
+            }
+
+        }
+            
+          function checkDiagonal(){
+            return checkWin({i: 1, j: -1}, {i: 1, j: 1});
+          }
+          function checkDiagonal1(){
+            return checkWin({i: 1, j: 1}, {i: -1, j: -1});
+          }
+          function checkVerticals() {
+            return checkWin({i: -1, j: 0}, {i: 1, j: 0});
+          }
+
+          function checkHorizontals() {
+            return checkWin({i: 0, j: -1}, {i: 0, j: 1});
+          }
+          return checkVerticals() || checkHorizontals() || checkDiagonal() || checkDiagonal1();
+    }
+
+
+    restart (){
+       this.createGrid();
+       this.onPlayerMove();
+    }
+}
---
+++ main.js
@@ -0,0 +1,17 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("dialog + button");
+const closeButton = document.querySelector("dialog button");
+
+// Le bouton "Afficher la fenêtre" ouvre le dialogue
+showButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+// Le bouton "Fermer" ferme le dialogue
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});
+});
+// console.log('ok')
+
